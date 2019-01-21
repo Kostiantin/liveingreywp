1bbf1319b472347261fcdb34bf6818db2fd4cab6
KostiantynZ <integrity1@mail.ru>
2019-01-21 10:20:08 +0200

    finished on wp

--- wp-content/themes/liveingreywp/header.php
+++ wp-content/themes/liveingreywp/header.php
@@ -15,7 +15,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
