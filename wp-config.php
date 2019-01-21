7dbfdfe88b0a610f7802121fed2a29a337de1bae
KostiantynZ <integrity1@mail.ru>
2019-01-21 10:46:27 +0200

    finished on wp

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'liveingreywp');
 define('DB_USER', 'liveingreywp');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'https://liveingreywp.frb.io');
+define('DB_PASSWORD', 'LM53jQoPmWYpfTpN_.tGHcdt');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
